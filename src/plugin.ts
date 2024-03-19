976ee5f8d9f8c72d1d134b77a456a9b8fc33e141
Alfinch <alfiewoodland@gmail.com>
2024-03-19 20:45:33 +0000

    Initial commit

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,19 +1,16 @@
-/// <reference path="../lib/openrct2.d.ts" />
-
-import { startup } from "./startup";
+import openMainWindow from "./open-main-window";
 
 registerPlugin({
-	name: "Name of your plugin",
-	version: "1.0",
-	authors: [ "Your name" ],
-	type: "remote",
-	licence: "MIT",
-	/**
-	 * This field determines which OpenRCT2 API version to use. It's best to always use the
-	 * latest release version, unless you want to use specific versions from a newer develop
-	 * version. Version 70 equals the v0.4.4 release.
-	 * @see https://github.com/OpenRCT2/OpenRCT2/blob/v0.4.4/src/openrct2/scripting/ScriptEngine.h#L50
-	 */
-	targetApiVersion: 70,
-	main: startup,
+  name: "Terrain generator",
+  version: "0.0.0",
+  authors: ["Alfie Woodland"],
+  type: "local",
+  licence: "MIT",
+  targetApiVersion: 77,
+  minApiVersion: 77,
+  main: main,
 });
+
+function main(): void {
+  ui.registerMenuItem("Terrain generator", openMainWindow);
+}
